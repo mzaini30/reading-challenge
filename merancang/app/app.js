a83e10b2c0020e30185973b811a3ac1b784e7be5
mzaini30 <muhzaini30@gmail.com>
2019-02-03 18:37:29 +0800

    oke

--- merancang/app/app.js
+++ merancang/app/app.js
@@ -9,28 +9,43 @@ update_persen = function(){
 	$('.progress-bar').attr('aria-valuenow', persen_selesai).css('width', persen_selesai + '%')
 	$('.alert-success').html(persen_selesai + '% Complete')
 }
-
 $('.navbar-nav li a').click(function(){
 	$('.navbar-toggle').click()
 })
 
-$('#myModal').modal()
+if (localStorage.getItem('reading challenge') == null){
+	$('#myModal').modal()
+	$('.progress').hide()
+} else {
+	$('.isi-target').html(localStorage.getItem('reading challenge'))
+	update_persen()
+}
 $('.buka-modal').click(function(){
 	$('#myModal').modal()
 })
 
 $('.count').click(function(){
-
-	// ----------------------------------------------------------------
-	// eksperimen
-	// ----------------------------------------------------------------
-
-	hasil_bagi = $('.total-pages').val() / $('.target-finish').val()
-	$('title').html(hasil_bagi)
+	hasil_bagi = Math.ceil($('.total-pages').val() / $('.target-finish').val())
+	// $('title').html(hasil_bagi)
+	list_halaman = []
+	for (x = hasil_bagi; x <= $('.total-pages').val(); x += hasil_bagi){
+		list_halaman.push(x)
+	}
+	if (list_halaman[list_halaman.length - 1] != $('.total-pages').val()){
+		list_halaman.push($('.total-pages').val())
+	}
+	$('.isi-target').html('')
+	for (x in list_halaman){
+		$('.isi-target').append('<tr><td class="cek-container"><div class="btn btn-default cek">&check;</div></td><td class="isi">Page '+list_halaman[x]+'</td></tr>')
+	}
+	$('.progress').show()
+	update_persen()
+	localStorage.setItem('reading challenge', $('.isi-target').html())
 })
 
 $(document).on('click', '.cek', function(){
 	$(this).toggleClass('btn-default').toggleClass('btn-success').toggleClass('cek-oke')
 	$(this).parent().parent().find('.isi').toggleClass('selesai')
 	update_persen()
+	localStorage.setItem('reading challenge', $('.isi-target').html())
 })
